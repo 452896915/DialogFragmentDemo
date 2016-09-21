007361b40e93ca42c91c13919c93de9784a455f1
wangdongliang <wangdongliang@chunyu.me>
2016-09-21 16:03:42 +0800

    加入了onCancel和onDismiss用法示例

--- app/src/main/java/com/magic/wdl/dialogdemo/MyDialogFragment.java
+++ app/src/main/java/com/magic/wdl/dialogdemo/MyDialogFragment.java
@@ -6,6 +6,7 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.Toast;
 
 /**
  * Created by wangdongliang on 16/9/20.
@@ -25,7 +26,7 @@ public class MyDialogFragment extends DialogFragment {
         // 设置Dialog样式和theme
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(), 0);
         builder.setTitle("提示")
- //             .setMessage("确定继续？")
+                //             .setMessage("确定继续？")
                 .setView(getActivity().getLayoutInflater().inflate(R.layout.custom_view, null))
                 .setPositiveButton("确定", new DialogInterface.OnClickListener() {
                     @Override
@@ -46,6 +47,44 @@ public class MyDialogFragment extends DialogFragment {
 
         Dialog dialog = builder.create();
 
+        /* 错误的用法
+        dialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
+                Toast.makeText(getActivity(), "Cancel now", Toast.LENGTH_SHORT).show();
+
+                dismiss();
+            }
+        });
+
+        dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialog) {
+                Toast.makeText(getActivity(), "Dismiss now", Toast.LENGTH_SHORT).show();
+
+                dismiss();
+            }
+        });
+        */
+
         return dialog;
+    }
+
+    /*
+    * 正确的用法
+    *
+    * */
+    @Override
+    public void onCancel(DialogInterface dialog) {
+        Toast.makeText(getActivity(), "Cancel now", Toast.LENGTH_SHORT).show();
+
+        super.onCancel(dialog);
+    }
+
+    @Override
+    public void onDismiss(DialogInterface dialog) {
+        Toast.makeText(getActivity(), "Dismiss now", Toast.LENGTH_SHORT).show();
+
+        super.onDismiss(dialog);
     }
 }
